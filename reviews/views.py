bf9bd2b5f51024426a184ec75e3e24d7ef0f1127
ccmmaa1030 <ccmmaa1030@gmail.com>
2022-10-21 15:46:01 +0900

    comments-index

--- reviews/views.py
+++ reviews/views.py
@@ -30,9 +30,11 @@ def index(request):
 def detail(request, review_pk):
     review = Review.objects.get(pk=review_pk)
     comment_form = CommentForm()
+    comments = review.comment_set.all()
     context = {
         'review': review,
-        'comment_form': comment_form
+        'comment_form': comment_form,
+        'comments': comments
     }
     return render(request, 'reviews/detail.html', context)
 
